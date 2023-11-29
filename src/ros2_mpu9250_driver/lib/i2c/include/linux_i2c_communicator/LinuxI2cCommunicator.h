790b8a6bd5bfcfad9134e19246451056222c8a6b
VitaliiKRS <userand130607@gmail.com>
2023-11-29 15:33:11 +0200

    Fix imu

--- src/ros2_mpu9250_driver/lib/i2c/include/linux_i2c_communicator/LinuxI2cCommunicator.h
+++ src/ros2_mpu9250_driver/lib/i2c/include/linux_i2c_communicator/LinuxI2cCommunicator.h
@@ -4,16 +4,16 @@
 #include "I2cCommunicator.h"
 
 class LinuxI2cCommunicator final : public I2cCommunicator {
- public:
-  LinuxI2cCommunicator(int bus_number = 1);
-  ~LinuxI2cCommunicator();
-  int read(unsigned char address) final;
-  int write(unsigned char address, unsigned char value) final;
-  char getFile() final;
+public:
+    LinuxI2cCommunicator(int bus_number = 8);
+    ~LinuxI2cCommunicator();
+    int read(unsigned char address) final;
+    int write(unsigned char address, unsigned char value) final;
+    char getFile() final;
 
- private:
-  void reportError(int error);
-  int file_;
+private:
+    void reportError(int error);
+    int file_;
 };
 
 #endif  // __LINUXI2CCOMMUNICATOR_H__
